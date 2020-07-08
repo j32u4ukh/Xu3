3066ab5e8ed92a1050a1b9d31a540833521f3858
j32u4ukh <j32u4ukh@gmail.com>
2020-07-08 08:29:41 +0800

    add getLogger

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import cv2
 import inspect
+import logging
 
 
 def showImage(*args):
@@ -29,3 +30,48 @@ def multiOperation(op, *args):
 def getFuncName():
     return inspect.stack()[1][3]
 
+
+def getLogger(logger_name, logger_level=logging.DEBUG, logger_format=None):
+    if logger_format is None:
+        logger_format = '%(asctime)s [%(name)s] %(levelname)s: %(funcName)s | ' \
+                        '%(message)s (%(pathname)s, line: %(lineno)d)'
+
+    # logger.debug("debug")
+    # logger.info("info")
+    # logger.warning("warning")
+    # logger.error("error")
+    # logger.critical("critical")
+
+    # logger_name: 設置 logger 名稱
+    logger = logging.getLogger(logger_name)
+
+    # 避免重複輸出 log
+    if not logger.handlers:
+        # 設置 logger 的 level
+        logger.setLevel(logger_level)
+
+        # 創建一個輸出日誌到控制台的 StreamHandler
+        handler = logging.StreamHandler()
+
+        # 設置 logger 的格式
+        formatter = logging.Formatter(logger_format)
+
+        # 將格式添加給 StreamHandler
+        handler.setFormatter(formatter)
+
+        # 將 handler 添加給 logger
+        logger.addHandler(handler)
+
+    return logger
+
+
+if __name__ == "__main__":
+    logger = getLogger("Xu3", logging.DEBUG)
+
+    def test():
+        print("Function")
+        # func_name = getFuncName()
+        logger.debug("Function")
+
+
+    test()
